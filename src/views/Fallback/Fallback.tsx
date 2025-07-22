fdcc93b3d653014a6a54f3cc70f2c45764aa05ee
nickoff <nick.olehnovich@gmail.com>
2025-07-22 21:23:55 +0300

    refactor: Fallback to function component

--- src/views/Fallback/Fallback.tsx
+++ src/views/Fallback/Fallback.tsx
@@ -1,22 +1,16 @@
-import { Component } from 'react';
 import './Fallback.css';
 
 interface FallbackProps {
   reloadCallback: () => void;
 }
 
-export class Fallback extends Component<FallbackProps> {
-  constructor(props: FallbackProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="fallback">
-        <h2>Ups... 😟 Something went wrong!</h2>
-        <button className="fallback__reload-button" onClick={this.props.reloadCallback}>
-          Try reload
-        </button>
-      </div>
-    );
-  }
-}
+export const Fallback = (props: FallbackProps) => {
+  return (
+    <div className="fallback">
+      <h2>Ups... 😟 Something went wrong!</h2>
+      <button className="fallback__reload-button" onClick={props.reloadCallback}>
+        Try reload
+      </button>
+    </div>
+  );
+};
