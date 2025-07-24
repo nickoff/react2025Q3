101e13f3df1b4f2def0a6a061c18456584c20ac1
nickoff <nick.olehnovich@gmail.com>
2025-07-24 12:09:47 +0300

    refactor: fix mentor coments

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
         'src/**/*.test.{js,jsx,ts,tsx}',
         'src/**/*.spec.{js,jsx,ts,tsx}',
         'src/index.{js,jsx,ts,tsx}',
+        'src/main.{js,jsx,ts,tsx}',
         'src/setupTests.{js,ts}',
         'src/**/*.d.ts'
       ],
