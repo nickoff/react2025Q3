bfda46647c0b8daa131fa95290e9ed65071a2ba6
nickoff <nick.olehnovich@gmail.com>
2025-07-30 23:02:38 +0300

    feat: implement switch theme style for About

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 export const NotFound = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-5 min-h-screen">
+    <div className="w-full flex flex-col justify-center items-center gap-5 min-h-[85vh]">
       <h3 className="text-8xl font-bold text-orange-600 text-shadow-amber-950">404: Page not found</h3>
       <Link
         className="text-2xl text-amber-50 px-4 py-1 rounded-md border border-black outline-none bg-gray-600"
