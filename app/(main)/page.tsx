99cbeb030ed84a433a08add0dceaa967ffcabd68
nickoff <nick.olehnovich@gmail.com>
2025-08-14 19:44:17 +0300

    feat: add content on home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,14 @@
+const CONTENT = {
+  title: 'Welcome, fellow otaku 🌸',
+  content:
+    'Select an anime from the list on the left to view its details. Use the search bar to find your favorite titles.',
+};
+
 export default function Page() {
-  return <></>;
+  return (
+    <div className="flex flex-col gap-5 items-center mt-28 text-center text-muted-foreground px-6">
+      <h2 className="text-8xl font-bold text-orange-600 text-shadow-amber-950">{CONTENT.title}</h2>
+      <p className="text-3xl">{CONTENT.content}</p>
+    </div>
+  );
 }
