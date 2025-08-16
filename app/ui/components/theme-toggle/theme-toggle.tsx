559381b5d53000ffdb130630bf12cf9a2de216fe
nickoff <nick.olehnovich@gmail.com>
2025-08-16 13:38:46 +0300

    refactor: ThemeToggle

--- app/ui/components/theme-toggle/theme-toggle.tsx
+++ app/ui/components/theme-toggle/theme-toggle.tsx
@@ -5,15 +5,24 @@ import { ThemeIcon } from './theme-icon/theme-icon';
 
 export const ThemeToggle = () => {
   const [themeDark, setThemeDark] = useState(false);
+
   const handleChangeTheme = () => {
     setThemeDark(!themeDark);
+    localStorage.setItem('themeDark', themeDark.toString());
   };
 
   return (
-    <button
-      className="cursor-pointer w-9 h-9 p-0.5 rounded-full border-2 border-gray-500 transition duration-300 ease-in-out hover:border-2 hover:border-orange-400"
-      onClick={handleChangeTheme}>
-      {themeDark ? <ThemeIcon variant="dark" /> : <ThemeIcon variant="light" />}
-    </button>
+    <div className="flex items-center justify-center border-1 p-1 gap-1 border-gray-500 rounded-full">
+      <button
+        className={`${themeDark ? 'bg-gray-300/20' : ''} cursor-pointer w-9 h-9 p-0.5 rounded-full border-2 border-transparent transition duration-300 ease-in-out hover:border-2 hover:border-orange-400/50`}
+        onClick={handleChangeTheme}>
+        <ThemeIcon variant="dark" />
+      </button>
+      <button
+        className={`${!themeDark ? 'bg-gray-300/20' : ''} cursor-pointer w-9 h-9 p-0.5 rounded-full border-2 border-transparent transition duration-300 ease-in-out hover:border-2 hover:border-orange-400/50`}
+        onClick={handleChangeTheme}>
+        <ThemeIcon variant="light" />
+      </button>
+    </div>
   );
 };
