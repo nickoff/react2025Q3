89a6087205ef0473174e0e80c397569e32b03e14
nickoff <nick.olehnovich@gmail.com>
2025-08-17 00:15:21 +0300

    feat: locale Snackbar

--- app/ui/components/snackbar.tsx
+++ app/ui/components/snackbar.tsx
@@ -4,16 +4,13 @@ import { useAppDispatch, useAppSelector } from '@/app/lib/hooks/useStoreHooks';
 import { clearSelectedCards } from '@/app/lib/reducers/selectedCards';
 import { downloadCSV } from '@/app/lib/utils/downloadCSV';
 import { Button } from '@/app/ui/components';
-
-const CONTENT = {
-  unselect: 'Unselect all',
-  downLoad: 'Download',
-};
+import { useTranslations } from 'next-intl';
 
 export const Snackbar = () => {
   const selectedCards = useAppSelector((state) => state.selectedCards.selectedCards);
   const numSelectedCards = selectedCards.length;
   const dispatch = useAppDispatch();
+  const t = useTranslations('Snackbar');
 
   const handelUnselect = () => {
     dispatch(clearSelectedCards());
@@ -28,10 +25,10 @@ export const Snackbar = () => {
   if (numSelectedCards) {
     return (
       <div className="text-2xl gap-2 rounded-md p-5 fixed bottom-10 right-20 flex flex-col bg-[#6a7282d5]">
-        <p>{`${numSelectedCards} ${numSelectedCards === 1 ? 'item is ' : 'items are '}selected`}</p>
+        <p>{`${numSelectedCards} ${numSelectedCards === 1 ? `${t('item')}` : `${t('items')}`}${t('selected')}`}</p>
         <div className="flex gap-2">
-          <Button onClick={handelUnselect}>{CONTENT.unselect}</Button>
-          <Button onClick={handelDownload}>{CONTENT.downLoad}</Button>
+          <Button onClick={handelUnselect}>{t('unselect_button')}</Button>
+          <Button onClick={handelDownload}>{t('download_button')}</Button>
         </div>
       </div>
     );
