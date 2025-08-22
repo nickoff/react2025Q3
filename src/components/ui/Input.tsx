dc9209488603e98ab5b191963b07c705c5ec8d2f
nickoff <nick.olehnovich@gmail.com>
2025-08-22 16:40:18 +0300

    feat: create shared Input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,75 @@
+import type { FieldError } from 'react-hook-form';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: 'text-input' | 'radio-list' | 'checkbox';
+  label: string;
+  id?: string;
+  error?: FieldError;
+  radioList?: { label: string; value: string }[];
+}
+
+export const Input = ({
+  variant = 'text-input',
+  label,
+  id,
+  className = '',
+  error,
+  radioList,
+  ...props
+}: InputProps) => {
+  const baseStyle = 'flex flex-col items-start w-full gap-2 w-full';
+
+  if (variant === 'text-input')
+    return (
+      <div className={`${baseStyle} ${className}`}>
+        <div className="flex items-center justify-between w-full">
+          <label htmlFor={id}>{label}</label>
+          {error && <span className="text-red-400">{error.message}</span>}
+        </div>
+        <input className="text-base p-2 bg-gray-500 outline-0 w-full" id={id} name={id} {...props} />
+      </div>
+    );
+
+  if (variant === 'radio-list')
+    return (
+      <div className={`${baseStyle} ${className}`}>
+        <div className="flex items-center justify-between w-full">
+          <legend>{label}</legend>
+          {error && <span className="text-red-400">{error.message}</span>}
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          {radioList &&
+            radioList.map((item, index) => {
+              return (
+                <>
+                  <label key={index} htmlFor={item.value}>
+                    {item.label}
+                  </label>
+                  <input
+                    key={index}
+                    className="text-base p-2 bg-gray-500 outline-0"
+                    id={item.value}
+                    value={item.value}
+                    type="radio"
+                    {...props}
+                  />
+                </>
+              );
+            })}
+        </div>
+      </div>
+    );
+
+  if (variant === 'checkbox')
+    return (
+      <div className={`${baseStyle} ${className}`}>
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center justify-start gap-2">
+            <input className="text-base p-2 bg-gray-500 outline-0" id={id} type="checkbox" {...props} />
+            <label htmlFor={id}>{label}</label>
+          </div>
+          {error && <span className="text-red-400">{error.message}</span>}
+        </div>
+      </div>
+    );
+};
--- src/components/ui/ReactHookForm.tsx
+++ src/components/ui/ReactHookForm.tsx
@@ -9,6 +9,7 @@ import { Button } from './Button';
 import { useAppDispatch } from '../../store/hooks';
 import { transformToDispatchModel } from '../../utils/transformToDispatchModel';
 import { submitForm } from '../../store/reducers/formSlice';
+import { Input } from './Input';
 
 interface ReactHookFormProps {
   onSuccess: () => void;
@@ -27,7 +28,7 @@ export const ReactHookForm = ({ onSuccess }: ReactHookFormProps) => {
     handleSubmit,
     watch,
     reset,
-    formState: { errors },
+    formState: { errors, isValid },
     setFocus,
   } = useForm<FormValidationSchema>({
     mode: 'onChange',
@@ -61,97 +62,67 @@ export const ReactHookForm = ({ onSuccess }: ReactHookFormProps) => {
       <form
         className="text-xs uppercase flex flex-col items-start w-full gap-2"
         onSubmit={handleSubmit(handleSubmitForm)}>
-        <div className="flex items-center justify-between w-full">
-          <label htmlFor="name">Name</label>
-          {errors.name && <span className="text-red-400">{errors.name.message}</span>}
-        </div>
-        <input className="text-base p-2 bg-gray-500 outline-0 w-full" id="name" type="text" {...register('name')} />
-        <div className="flex items-center justify-between w-full">
-          <label htmlFor="age">Age</label>
-          {errors.age && <span className="text-red-400">{errors.age.message}</span>}
-        </div>
-        <input className="text-base p-2 bg-gray-500 outline-0  w-full" id="age" type="text" {...register('age')} />
-        <div className="flex items-center justify-between w-full">
-          <label htmlFor="email">Email</label>
-          {errors.email && <span className="text-red-400">{errors.email.message}</span>}
-        </div>
-        <input className="text-base p-2 bg-gray-500 outline-0 w-full" id="email" type="email" {...register('email')} />
-        <div className="flex items-center justify-between w-full">
-          <label htmlFor="newPassword">New password</label>
-          {errors.newPassword && <span className="text-red-400">{errors.newPassword.message}</span>}
-        </div>
-        <input
-          className="text-base p-2 bg-gray-500 outline-0 w-full"
+        <Input variant="text-input" label="Name" error={errors.name} id="name" type="text" {...register('name')} />
+        <Input variant="text-input" label="Age" error={errors.age} id="age" type="text" {...register('age')} />
+        <Input variant="text-input" label="Email" error={errors.email} id="email" type="email" {...register('email')} />
+        <Input
+          variant="text-input"
+          label="New password"
+          error={errors.newPassword}
           id="newPassword"
           type="password"
           {...register('newPassword')}
         />
         <PasswordStrengthBar password={password} />
-        <div className="flex items-center justify-between w-full">
-          <label htmlFor="confirmPassword">Confirm password</label>
-          {errors.confirmPassword && <span className="text-red-400">{errors.confirmPassword.message}</span>}
-        </div>
-        <input
-          className="text-base p-2 bg-gray-500 outline-0 w-full"
+        <Input
+          variant="text-input"
+          label="Confirm password"
+          error={errors.confirmPassword}
           id="confirmPassword"
           type="password"
           {...register('confirmPassword')}
         />
-        <div className="flex items-center justify-between w-full">
-          <legend>Gender</legend>
-          {errors.gender && <span className="text-red-400">{errors.gender.message}</span>}
-        </div>
-        <div className="flex items-center justify-start gap-2">
-          <label htmlFor="female">female</label>
-          <input
-            className="text-base p-2 bg-gray-500 outline-0"
-            id="female"
-            value="female"
-            type="radio"
-            {...register('gender')}
-          />
-          <label htmlFor="male">male</label>
-          <input
-            className="text-base p-2 bg-gray-500 outline-0"
-            id="male"
-            value="male"
-            type="radio"
-            {...register('gender')}
-          />
-        </div>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center justify-start gap-2">
-            <input
-              className="text-base p-2 bg-gray-500 outline-0"
-              id="accept"
-              type="checkbox"
-              {...register('accept')}
-            />
-            <label htmlFor="accept">Accept Terms and Conditions agreement</label>
-          </div>
-          {errors.accept && <span className="text-red-400">{errors.accept.message}</span>}
-        </div>
-        <div className="flex items-center justify-between w-full">
-          <label htmlFor="upload">Upload picture</label>
-          {errors.upload && <span className="text-red-400">{errors.upload.message}</span>}
-        </div>
-        <input className="text-base p-2 bg-gray-500 outline-0 w-full" id="upload" type="file" {...register('upload')} />
-        <div className="flex items-center justify-between w-full">
-          <label htmlFor="country">Select country</label>
-          {errors.country && <span className="text-red-400">{errors.country.message}</span>}
-        </div>
-        <input
-          className="text-base p-2 bg-gray-500 outline-0 w-full"
+        <Input
+          variant="radio-list"
+          label="Gender"
+          error={errors.gender}
+          radioList={[
+            { label: 'Male', value: 'male' },
+            { label: 'Female', value: 'female' },
+          ]}
+          {...register('gender')}
+        />
+        <Input
+          variant="checkbox"
+          label="Accept Terms and Conditions agreement"
+          error={errors.accept}
+          id="accept"
+          {...register('accept')}
+        />
+        <Input
+          variant="text-input"
+          label="Upload picture"
+          error={errors.upload}
+          id="upload"
+          type="file"
+          {...register('upload')}
+        />
+        <Input
+          variant="text-input"
+          label="Select country"
+          error={errors.country}
           id="country"
           list="country-list"
           {...register('country')}
         />
         <datalist id="country-list">
-          {isFetching && 'Loading...'}
+          {isFetching && <option value="Loading..."></option>}
           {data && data.map((country, index) => <option key={index} value={country}></option>)}
         </datalist>
         <div className="flex items-center justify-center gap-5">
-          <Button type="submit">Submit</Button>
+          <Button type="submit" disabled={!isValid}>
+            Submit
+          </Button>
           <Button variant="danger" type="button" onClick={handleResetForm}>
             Reset form
           </Button>
