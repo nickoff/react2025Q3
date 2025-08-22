9579fc60b11a94656c46ea5b344caa5e985449ae
nickoff <nick.olehnovich@gmail.com>
2025-08-22 20:37:18 +0300

    feat: add FormSubmissionList

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,18 +1,14 @@
 import { Footer } from '../components/ui/Footer';
-import { useAppSelector } from '../store/hooks';
+import { FormSubmissionList } from '../components/ui/FormSubmissionList';
 import { ModalSwitchWidget } from '../components/ui/ModalSwitchWidget';
-import { selectFormLastIdValue, selectFormSubmissionsValue } from '../store/reducers/formSlice';
 
 export const Layout = () => {
-  const formList = useAppSelector(selectFormSubmissionsValue);
-  const lastFormId = useAppSelector(selectFormLastIdValue);
-  console.log(lastFormId, formList);
-
   return (
     <div className="text-2xl max-w-7xl h-[100vh] mx-auto flex flex-col">
       <header className="py-9 uppercase font-bold">React2025Q3</header>
       <main className="w-full h-full flex flex-col justify-between">
         <ModalSwitchWidget />
+        <FormSubmissionList />
       </main>
       <Footer />
     </div>
