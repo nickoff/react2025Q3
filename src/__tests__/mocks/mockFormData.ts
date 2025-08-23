f967a01fbd598cd77b3113230c715efc711fab3b
nickoff <nick.olehnovich@gmail.com>
2025-08-23 18:55:25 +0300

    test: add test transformToDispatchModel

---
+++ src/__tests__/mocks/mockFormData.ts
@@ -0,0 +1,21 @@
+import type { FormInputModel } from '../../types/form.type';
+
+const allowedPassword = 'Nick!1';
+const allowedFile = new File(['dummy'], 'avatar.png', { type: 'image/png' });
+const input = document.createElement('input');
+input.setAttribute('type', 'file');
+const mockFileList = Object.create(input.files);
+mockFileList[0] = allowedFile;
+Object.defineProperty(mockFileList, 'length', { value: 1 });
+
+export const mockFormDataValid = {
+  name: 'Nickolas',
+  age: '41',
+  email: 'test@exampe.com',
+  newPassword: allowedPassword,
+  confirmPassword: allowedPassword,
+  gender: 'male',
+  accept: true,
+  upload: mockFileList,
+  country: 'Belarus',
+} as FormInputModel;
--- src/__tests__/zod-schema.test.ts
+++ src/__tests__/zod-schema.test.ts
@@ -1,28 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import { createSchema } from '../utils/zod.schema';
+import { mockFormDataValid } from './mocks/mockFormData';
 
 describe('Zod Schema', () => {
   const allowedCountries = ['Belarus', 'Poland'];
   const allowedPassword = 'Nick!1';
-  const allowedFile = new File(['dummy'], 'avatar.png', { type: 'image/png' });
-  const input = document.createElement('input');
-  input.setAttribute('type', 'file');
-  const mockFileList = Object.create(input.files);
-  mockFileList[0] = allowedFile;
-  Object.defineProperty(mockFileList, 'length', { value: 1 });
 
   it('passes if valid data', () => {
-    const result = createSchema(allowedCountries, allowedPassword).safeParse({
-      name: 'Nickolas',
-      age: '41',
-      email: 'test@exampe.com',
-      newPassword: allowedPassword,
-      confirmPassword: allowedPassword,
-      gender: 'male',
-      accept: true,
-      upload: mockFileList,
-      country: 'Belarus',
-    });
+    const result = createSchema(allowedCountries, allowedPassword).safeParse(mockFormDataValid);
 
     expect(result.success).toBe(true);
   });
