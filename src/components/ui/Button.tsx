15e29c72fb7109827bcc7a0dd97916567b85605c
nickoff <nick.olehnovich@gmail.com>
2025-08-24 20:14:39 +0300

    style(Button): disable

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ variant = 'primary', children, className = '', ...props
   const baseStyle = 'p-3 min-w-50 uppercase font-bold border-2 rounded-md cursor-pointer disabled:cursor-not-allowed';
 
   const variantsStyle = {
-    primary: 'bg-cyan-700 border-cyan-800 hover:bg-cyan-800 disabled:bg-cyan-700/30',
+    primary: 'bg-cyan-700 border-cyan-800 hover:bg-cyan-800 disabled:bg-gray-700/30',
     danger: 'bg-red-700 border-red-800 hover:bg-red-800 disabled:bg-red-700/30',
   };
 
