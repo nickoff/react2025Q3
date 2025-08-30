0c4fb65cb99298de58dbb05070ab1901fb454d4e
nickoff <nick.olehnovich@gmail.com>
2025-08-30 08:56:56 +0300

    feat: create Header component

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="py-9 uppercase font-bold flex justify-between">
+      <h2>React2025Q3</h2>
+      <h3 className="font-normal">Task: Performance</h3>
+    </header>
+  );
+};
